80f8fc87d755c11e2d28b183e4639d604b46dafd
berdjabrahamian <berdj.abrahamian@gmail.com>
2020-03-03 19:53:45 -0500

    Posts Model - Part 2

--- app/models/post.js
+++ app/models/post.js
@@ -1,4 +1,4 @@
-import Model, { attr } from '@ember-data/model';
+import Model, {attr} from '@ember-data/model';
 
 export default class PostModel extends Model {
 
@@ -11,4 +11,15 @@ export default class PostModel extends Model {
   @attr('date') createdAt;
   @attr('date') updatedAt;
   @attr('date') publishedAt;
+
+  //We will get the created at and separate it into MM/DD/YYYY so we can create a craphic of when it was posted
+  get postedDate() {
+    let date = new Date(this.createdAt);
+
+
+    return [
+      date.toLocaleDateString('default', {day: '2-digit'}),
+      date.toLocaleDateString('default', {month: 'short'}),
+      date.toLocaleDateString('default', {year: 'numeric'})];
+  }
 }
