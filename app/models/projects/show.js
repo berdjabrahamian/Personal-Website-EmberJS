fd4682782fab9fa46e2b87db4cd3d4b016430694
berdjabrahamian <berdj.abrahamian@gmail.com>
2020-03-22 17:29:00 -0400

    Updated project show page template

--- app/models/projects/show.js
+++ app/models/projects/show.js
@@ -5,11 +5,14 @@ export default class ProjectsShowModel extends Model {
   @attr full_name;
   @attr('boolean') private;
   @attr owner;
-  @attr('date') createdAt;
-  @attr avatar;
   @attr html_url;
-  @attr status;
   @attr description;
   @attr ('boolean') fork;
-
+  @attr status;
+  @attr ('date') created_at;
+  @attr ('date') updated_at;
+  @attr ('date') pushed_at;
+  @attr ssh_url;
+  @attr clone_url;
+  @attr ('string') language;
 }
